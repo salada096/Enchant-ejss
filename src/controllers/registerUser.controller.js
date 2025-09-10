fdf27471f28599dd1218713522ee070dbe49bb84
Guilherme Oliver <guilherme.oliver@msn.com>
2025-09-10 17:19:13 -0300

    Pagina de cadastro do Doador Integrada

--- src/controllers/registerUser.controller.js
+++ src/controllers/registerUser.controller.js
@@ -1,23 +1,70 @@
-import { userRegisterDoador, userRegisterDonatario, userRegisterInstituicao, userRegisterOng } from "../services/registerUser.service.js";
+import pool from "../db/pool.db.js";
+import path from "path";
+import { userRegisterPessoaFisica, userRegisterDonatario, userRegisterInstituicao, userRegisterOng } from "../services/registerUser.service.js";
 
 async function registerUserDoador(req, res) {
     
-    console.log(`📦   Dados recebidos: ${JSON.stringify(req.body)}`);
+    console.log(`📦   Dados recebidos: ${JSON.stringify(req.body, null, 2)}`);
 
     try{
 
-        const {} = req.body;
+        const { 
+            nomeCompleto, 
+            email, 
+            tipo_doador, 
+            senha, 
+            pessoaCpf, 
+            pessoaRg, 
+            pessoaTelefone, 
+            ongCnpj, 
+            ongTelefone, 
+            ongAno,
+            instituicaoTipo,
+            instituicaoCnpj, 
+            instituicaoTelefone, 
+            instituicaoCep, 
+            instituicaoBairro
+        } = req.body;
+
+        await pool.query('BEGIN');
+
+        if(tipo_doador === "pessoa_fisica"){
+            
+            console.log(`\n⌛   Iniciando o cadastro de Pessoa Física   ⌛\n`);
+
+            const novoDoador = await userRegisterPessoaFisica({ nomeCompleto, email, tipo_doador, senha, pessoaCpf, pessoaRg, pessoaTelefone });
+
+        }else if(tipo_doador === "ong"){
+
+            console.log(`\n⌛   Iniciando o cadastro de Organização Não Governamental (ONG)   ⌛\n`);
+
+            const novoDoador = await userRegisterOng({ nomeCompleto, email, tipo_doador, senha, ongCnpj, ongTelefone, ongAno });
+
+        }else{
+
+            console.log(`\n⌛   Iniciando o cadastro de Instituição   ⌛\n`);
+
+            const novoDoador = await userRegisterInstituicao({ nomeCompleto, email, tipo_doador, senha, instituicaoTipo, instituicaoCnpj, instituicaoTelefone, instituicaoCep, instituicaoBairro });
+
+        }
+
+        await pool.query('COMMIT');
+        console.log(`\n✅   Cadastro de ${tipo_doador} realizado com sucesso   ✅\n`);
+        return res.status(201).redirect('/');
 
     }catch (error){
+
+        await pool.query('ROLLBACK');
         console.error(`❌   Erro na transação: ${error}   ❌`);
-        throw new Error('➡️   Erro ao registrar usuário Doador   ⬅️');
+        throw error;
+
     };
 
 };
 
 async function registerUserDonatario(req, res) {
     
-    console.log(`📦   Dados recebidos: ${JSON.stringify(req.body)}`);
+    console.log(`📦   Dados recebidos: ${JSON.stringify(req.body, null, 2)}`);
 
     try{
 
@@ -25,7 +72,7 @@ async function registerUserDonatario(req, res) {
 
     }catch (error){
         console.error(`❌   Erro na transação: ${error}   ❌`);
-        throw new Error('➡️   Erro ao registrar usuário Doador   ⬅️');
+        throw error;
     };
 
 };
