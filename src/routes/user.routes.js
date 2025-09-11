1221f008a6f93d42facf95b98fe5b137b2fe344f
Andressa-gith <andressa.cardoso@ba.estudante.senai.br>
2025-09-11 17:50:05 -0300

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,4 +12,26 @@ userRouter.post('/cadastrar/donatario', upload.none(), registerUserDonatario);
 
 userRouter.post('/login', login);
 
+//Esqueciasenha
+
+userRouter.post('/forgot-password', async (req, res, next) => {
+  const { handleForgotPassword } = await import('../controllers/forgotPassword.controller.js');
+  return handleForgotPassword(req, res, next);
+});
+
+userRouter.post('/verify-code', async (req, res, next) => {
+  const { handleVerifyCode } = await import('../controllers/forgotPassword.controller.js');
+  return handleVerifyCode(req, res, next);
+});
+
+userRouter.post('/resend-code', async (req, res, next) => {
+  const { handleResendCode } = await import('../controllers/forgotPassword.controller.js');
+  return handleResendCode(req, res, next);
+});
+
+userRouter.post('/reset-password', async (req, res, next) => {
+  const { handleResetPassword } = await import('../controllers/forgotPassword.controller.js');
+  return handleResetPassword(req, res, next);
+});
+
 export default userRouter;
